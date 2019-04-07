d7c5ade248231f4827188e2695bcd599f3974290
ito-tai <hanshin.abingdon@gmail.com>
2019-04-07 18:51:50 +0900

    add project

---
+++ Src/Loto7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Loto7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
