4ff77d6b2a9653515b2a595d97cdcd2997ede6ad
ito-tai <hanshin.abingdon@gmail.com>
2019-04-14 11:21:18 +0900

    get hit number columns

--- Src/Loto7/Models/HtmlGetter.cs
+++ Src/Loto7/Models/HtmlGetter.cs
@@ -14,13 +14,26 @@ namespace Loto7.Models
             var doc = Get(url).Result;
 
             // 対象のテーブルは一つしかない
-            var table = doc.GetElementsByClassName("bun_box2").First();
+            var table = doc
+                .GetElementsByClassName("bun_box2")
+                .First()
+                .GetElementsByTagName("tbody")
+                .Single();
+
+            // ヘッダを除いた当選情報を取得
+            var data = table
+                .GetElementsByTagName("tr")
+                .Where(x => !(x.GetElementsByTagName("th").Any()));
+
+            foreach (var tr in data)
+            {
+            }
 
             return doc;
         }
 
         /// <summary>
-        ///     html取得実態
+        /// html取得実態
         /// </summary>
         /// <param name="url"></param>
         /// <returns></returns>
